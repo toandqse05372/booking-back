b90ddb898a77ea92663f98bdc8f7f95e41681df9
- <->
2020-06-29 16:10:09 +0700

    hieunv_visitorType

---
+++ src/main/java/com/capstone/booking/common/converter/VisitorTypeConverter.java
@@ -0,0 +1,47 @@
+package com.capstone.booking.common.converter;
+
+import com.capstone.booking.entity.TicketType;
+import com.capstone.booking.entity.VisitorType;
+import com.capstone.booking.entity.dto.TicketTypeDTO;
+import com.capstone.booking.entity.dto.VisitorTypeDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VisitorTypeConverter {
+
+    @Autowired
+    private TicketTypeConverter typeConverter;
+
+    public VisitorType toVisitorType(VisitorTypeDTO dto) {
+        VisitorType visitorType = new VisitorType();
+        visitorType.setTypeName(dto.getTypeName());
+        visitorType.setTypeKey(dto.getTypeKey());
+        visitorType.setPrice(dto.getPrice());
+        return visitorType;
+    }
+
+    public VisitorType toVisitorType(VisitorTypeDTO dto, VisitorType visitorType) {
+        visitorType.setTypeName(dto.getTypeName());
+        visitorType.setTypeKey(dto.getTypeKey());
+        visitorType.setPrice(dto.getPrice());
+        return visitorType;
+    }
+
+    public VisitorTypeDTO toDTO(VisitorType visitorType) {
+        VisitorTypeDTO dto = new VisitorTypeDTO();
+        if (visitorType.getId() != null) {
+            dto.setId(visitorType.getId());
+        }
+        dto.setTypeName(visitorType.getTypeName());
+        dto.setTypeKey(visitorType.getTypeKey());
+        dto.setPrice(visitorType.getPrice());
+
+        TicketTypeDTO typeDTO = new TicketTypeDTO();
+        TicketType type = visitorType.getTicketType();
+        typeDTO = typeConverter.toDTO(type);
+        dto.setTicketType(typeDTO);
+
+        return dto;
+    }
+}
---
+++ src/main/java/com/capstone/booking/repository/VisitorTypeRepository.java
@@ -0,0 +1,7 @@
+package com.capstone.booking.repository;
+
+import com.capstone.booking.entity.VisitorType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VisitorTypeRepository extends JpaRepository<VisitorType, Long> {
+}
