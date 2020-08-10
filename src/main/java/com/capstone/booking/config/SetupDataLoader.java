325a4e7effaa28042410df8e7221099490bf9ba0
toandqse05372 <quangtoandao123@gmail.com>
2020-08-10 17:32:50 +0700

    toandq category description

--- src/main/java/com/capstone/booking/config/SetupDataLoader.java
+++ src/main/java/com/capstone/booking/config/SetupDataLoader.java
@@ -49,7 +49,7 @@ public class SetupDataLoader implements
 
     //add first category
     public void initCategory(){
-        if(categoryRepository.findOneByTypeKey("PARK") == null){
+        if(categoryRepository.findByTypeName("Khu vui chơi") == null){
             Category category = new Category("Khu vui chơi", "PARK", "Cho trẻ tha hồ chạy nhảy, leo trẻo khắp mọi nơi");
             categoryRepository.save(category);
         }
