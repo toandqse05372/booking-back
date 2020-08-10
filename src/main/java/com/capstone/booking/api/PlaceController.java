345731806bcc6993cb3797270c90142dcbd86632
toandqse05372 <quangtoandao123@gmail.com>
2020-08-10 21:03:34 +0700

    toandq category description

--- src/main/java/com/capstone/booking/api/PlaceController.java
+++ src/main/java/com/capstone/booking/api/PlaceController.java
@@ -99,4 +99,10 @@ public class PlaceController {
         return new ResponseEntity("Delete Successful", HttpStatus.OK);
     }
 
+    //getTop8ByCityId
+    @GetMapping("/topPlace")
+    public ResponseEntity<?> getTop8PlaceByCityId(@RequestParam(value = "cityId", required = false) Long cityId) {
+        return placeService.getTop8PlaceByCityId(cityId);
+    }
+
 }
